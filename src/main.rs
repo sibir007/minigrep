eb05e7222068503d76f461580f7a4641321d3d6a
Dmitry Sibiryakov <sibiriakoff2006@yandex.ru>
2025-03-04 00:15:16 +0300

    add read_to_string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    // dbg!(args);
+    let contents = fs::read_to_string(filename).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
