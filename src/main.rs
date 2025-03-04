0283613ea2853fdb6df3203c266b7b24ebce011c
sibir007 <sibriakoff2006@yandex.ru>
2025-03-04 07:48:43 +0300

    удалил лишннее

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+
+    let (query, file_path) = parse_config(&args);
+    
     println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("In file {}", file_path);
     // dbg!(args);
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
