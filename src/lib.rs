db502fd45c02c3723f1014689cbebad702ee77a5
sibir007 <sibriakoff2006@yandex.ru>
2025-03-07 14:57:28 +0300

    change indexing to iterations

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,7 @@ use std::error::Error;
 use std::env;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.file_path);
-    // dbg!(args);
+
     let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.case_sensitive {
@@ -31,13 +29,21 @@ pub struct Config {
 
 impl Config {
 
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return  Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        args.next();
+
+        let query: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path: String = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+
         let case_sensitive = env::var("CASE_SENSITIVE").is_ok();
         
         Ok(Config { query, file_path, case_sensitive })
@@ -47,31 +53,15 @@ impl Config {
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     
-    let mut results: Vec<&str>  = Vec::new();
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-            // println!("{}", line);
-        }
-        
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     
-    let query = query.to_lowercase();
-    let mut results: Vec<&str>  = Vec::new();
-    
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-            // println!("{}", line);
-        }
-        
-    }
-    results
+    let query: &str = &query.to_lowercase();
+    contents.lines()
+    .filter(|line| line.to_lowercase().contains(query))
+    .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
-use std::env;
+use std::env::{self, Args};
 use std::process;
+
 use minigrep::run;
 use minigrep::Config;
 // use std::String;
 
-fn main(){
-    let args: Vec<String> = env::args().collect();
+// fn main(){
+//     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+//     let config: Config = Config::build(&args).unwrap_or_else(|err| {
+//         eprintln!("Problem parsing arguments: {err}");
+//         process::exit(1);
+//     });
+
+//     if let Err(e) = run(config) {
+//         eprintln!("Application error: {e}");
+//         process::exit(1);
+//     }
+    
+// }
+
+fn main(){
+    let args: Args = env::args();
+
+    let config: Config = Config::build(args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
